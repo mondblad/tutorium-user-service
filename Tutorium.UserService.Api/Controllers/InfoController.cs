fbf90778cf2e929c366a7d31493a489a5231023d
mondblad <mondblad@gmail.com>
2025-10-25 23:39:32 +0300

    init

---
+++ Tutorium.UserService.Api/Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tutorium.UserService.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfoController : ControllerBase
+    {
+        /// <summary>
+        /// Получение информации
+        /// </summary>
+        [HttpGet(template: "version")]
+        public async Task<ActionResult<string>> GetCounterObjectMeasuringValues()
+        {
+            return "User Service 1.1";
+        }
+    }
+}
